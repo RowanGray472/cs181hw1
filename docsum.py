34120b3b9e8c180df0b7291e8458fc2f3d13b958
RowanGray472 <rgray26@cmc.edu>
2024-09-08 14:59:29 -0700

    too much to describe

--- docsum.py
+++ docsum.py
@@ -8,27 +8,29 @@ TERMINAL COMMANDS WE NEED TO RUN THIS CODE:
 
 """
 
-def split_document_into_chunks(text):
-    r'''
-    Split the input text into smaller chunks so that an LLM can process those chunks individually.
+def split_document_into_tokens(text, max_tokens=4000):
+    """
+    Split the input text into smaller chunks based on the number of tokens.
+    Each chunk contains up to `max_tokens` tokens.
+    """
+    words = text.split()
+    chunks = []
+    current_chunk = []
 
-    >>> split_document_into_chunks('This is a sentence.\n\nThis is another paragraph.')
-    ['This is a sentence.', 'This is another paragraph.']
-    >>> split_document_into_chunks('This is a sentence.\n\nThis is another paragraph.\n\nThis is a third paragraph.')
-    ['This is a sentence.', 'This is another paragraph.', 'This is a third paragraph.']
-    >>> split_document_into_chunks('This is a sentence.')
-    ['This is a sentence.']
-    >>> split_document_into_chunks('')
-    []
-    >>> split_document_into_chunks('This is a sentence.\n')
-    ['This is a sentence.']
-    >>> split_document_into_chunks('This is a sentence.\n\n')
-    []
-    >>> split_document_into_chunks('This is a sentence.\n\nThis is another paragraph.\n\n')
-    ['This is a sentence.', 'This is another paragraph.']''
+    for word in words:
+        # +1 for the space or newline that will follow the word
+        # *2 because each word is assumed to have, on average, two tokens
+        if (len(current_chunk) * 2) + len(word) + 1 > max_tokens:
+            chunks.append(' '.join(current_chunk))
+            current_chunk = [word]
+        else:
+            current_chunk.append(word)
 
-    '''
-    return text.split('\n\n')
+    # Don't forget the last chunk
+    if current_chunk:
+        chunks.append(' '.join(current_chunk))
+
+    return chunks
 
 
 if __name__ == '__main__':
@@ -42,7 +44,7 @@ if __name__ == '__main__':
     parser.add_argument('filename')
     args = parser.parse_args()
 
-# line 7+8 => args.filename will contain the first string after program name on command line
+    # line 7+8 => args.filename will contain the first string after program name on command line
     
     from dotenv import load_dotenv
     load_dotenv()
@@ -52,8 +54,30 @@ if __name__ == '__main__':
         api_key=os.environ.get("GROQ_API_KEY"),
     )
 
-    with open(args.filename) as f:
-        text = f.read()
+    from bs4 import BeautifulSoup
+    from pdfminer.high_level import extract_text
+    import chardet
+
+    def read_file(filename):
+        with open(filename, 'rb') as f:
+            result = chardet.detect(f.read())
+            charenc = result['encoding']
+
+        if filename.endswith('.html'):
+            with open(filename, 'r', encoding=charenc) as f:
+                soup = BeautifulSoup(f, 'html.parser')
+                text = soup.get_text()
+        elif filename.endswith('.pdf'):
+            text = extract_text(filename)
+        else:
+            with open(filename, 'r', encoding=charenc) as f:
+                text = f.read()
+        return text
+
+    text = read_file(args.filename)
+
+
+    print("file is read")
 
     '''
     We need to call the split_document_into_chunks on text.
@@ -64,17 +88,45 @@ if __name__ == '__main__':
     Recall the LLM code below on the new smaller document.
     '''
 
+    summary_list = []
+
+    chunks = split_document_into_tokens(text)
+
+    print("file is split into chunks")
+    counter = 0
+
+    for chunk in chunks:
+
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    'role': 'system',
+                    'content': 'Summarize the input text below.  Limit the summary to 1 paragraph and use a 1st grade reading level.',
+                },
+                {
+                    "role": "user",
+                    "content": chunk,
+                }
+            ],
+            model="llama3-8b-8192",
+        )
+        summary_list.append(chat_completion.choices[0].message.content)
+        counter += 1
+        print(f"chunk {counter} of {len(chunks)}is summarized")
+
+    summary = ' '.join(summary_list)
+
     chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                'role': 'system',
-                'content': 'Summarize the input text below.  Limit the summary to 1 paragraph and use a 1st grade reading level.',
-            },
-            {
-                "role": "user",
-                "content": text,
-            }
-        ],
-        model="llama3-8b-8192",
-    )
+            messages=[
+                {
+                    'role': 'system',
+                    'content': 'Summarize the input text below.  Limit the summary to 1 paragraph and use a 1st grade reading level.',
+                },
+                {
+                    "role": "user",
+                    "content": summary,
+                }
+            ],
+            model="llama3-8b-8192",
+        )
     print(chat_completion.choices[0].message.content)
